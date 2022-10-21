ff4d0e6b13df1e295d9666c5a1c59d0b7d350046
HansPuente <hpuentej@uni.pe>
2022-10-21 02:12:16 -0500

    actualizar contadorHook

--- src/estudioConJonmircha/contadorHook.js
+++ src/estudioConJonmircha/contadorHook.js
@@ -1,23 +1,29 @@
-import {useState} from 'react'
+import { useState } from "react";
 
-function Boton({signo, handleClickOne}){
-  return(
+function Boton({ signo, handleClickOne }) {
+  return (
     <>
-      <button onClick={() => handleClickOne(signo) } className="bg-cyan-800 rounded-lg p-1"> {signo}</button>
+      <button
+        onClick={() => handleClickOne(signo)}
+        className="bg-cyan-800 rounded-lg p-1"
+      >
+        {" "}
+        {signo}
+      </button>
     </>
-  )
+  );
 }
 
 export function Contador() {
-  
   const [contador, setContador] = useState(0);
 
   const handleClickOne = (oper) => {
-    if(oper === 'MAS 1'){
-    setContador(contador + 1)}
-    else if(oper === 'MENOS 1'){
-      setContador(contador - 1)}
-  }
+    if (oper === "MAS 1") {
+      setContador(contador + 1);
+    } else if (oper === "MENOS 1") {
+      setContador(contador - 1);
+    }
+  };
 
   return (
     <>
@@ -25,6 +31,5 @@ export function Contador() {
       <Boton signo="MAS 1" handleClickOne={handleClickOne} />
       <Boton signo="MENOS 1" handleClickOne={handleClickOne} />
     </>
-  )
+  );
 }
-
